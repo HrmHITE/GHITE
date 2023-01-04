69f0a10f89e087a6828d72411391a5f00df4b3ae
HrmHITE <hm@hitedigital.com>
2023-01-04 16:52:31 -0600

    Create function-icons-fontawesome-cdn.php
    Funcion que nos permite importar los iconos de FontAwesome CDN

---
+++ function-icons-fontawesome-cdn.php
@@ -0,0 +1,8 @@
+<?php
+//Function Icons FontAwesome CDN
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+function enqueue_load_fa() {
+wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+}
+//End Function
+?>
